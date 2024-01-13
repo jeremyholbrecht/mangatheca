cf84156cc03e7f3ce4437065c0d30ebda058766d
jeremyholbrecht <Griffley@proton.me>
2024-01-13 19:08:50 +0100

    trying out structs

--- main.go
+++ main.go
@@ -13,4 +13,7 @@ func main() {
 	gabaiVol1.name = "Gabai Volume 1"
 	fmt.Printf("%+v\n", gabaiVol1)
 	fmt.Println(Manga{"foo", "bar"})
+
+	crowsVolume1 := Manga{"Crows", "Hiroshi Takahashi"}
+	fmt.Println(crowsVolume1)
 }
