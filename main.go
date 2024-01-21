01819815583b78104622ce19b19cc4785ee40366
jeremyholbrecht <Griffley@proton.me>
2024-01-21 18:47:03 +0100

    first commit

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	fmt.Println(Manga{"foo", "bar"})
 	fmt.Println(gabaiVol1.name)
 	fmt.Println(gabaiVol1.author)
+	fmt.Println(gabaiVol1.name)
+	fmt.Println(gabaiVol1.author)
 
 	crowsVolume1 := Manga{"Crows", "Hiroshi Takahashi"}
 	crowsVolume2 := Manga{"Crows", "Hiroshi Takahashi"}
